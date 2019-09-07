65dd79335df749a1f78f7ce31a845f9314b0d79b
Andrew Lin <andrewlin618@gmail.com>
2019-09-06 21:36:43 -0700

    update: Basic frame done;

---
+++ javascript/mainGame.js
@@ -0,0 +1,20 @@
+var questionNow = "";
+var answerNowA = ""; 
+var answerNowB = ""; 
+var answerNowC = ""; 
+var answerNowD = ""; 
+var started = false;
+
+var indexOfQa = 0;
+
+//press start ,game start;
+
+$("#question").text(qaBank[0]);
+console.log(qaBank[0]);
+
+$("#answer-a").on("click", function (){
+    
+
+
+
+});
---
+++ javascript/questionLibrary.js
@@ -0,0 +1,29 @@
+var instruction = "这个游戏要怎么玩呢？？啊啊啊问的好啊！"
+
+var qa1 = {
+    question:"Question 1",
+    answer:["answer1a","answer1b","answer1c","answer1d"],
+    correctAnswer:"answerB"
+}
+var qa2 = {
+    question: "Question 2",
+    answer:["answer2a","answer2b","answer2c","answer2d"],
+    correctAnswer:"answerB"
+}
+
+var qa3 = {
+    question: "Question 3",
+    answer:["answer3a","answer3b","answer3c","answer3d"],
+    correctAnswer:"answerB"
+}
+
+var qa4 = {
+    question: "Question 4",
+    answer:["answer4a","answer4b","answer4c","answer4d"],
+    correctAnswer:"answerB"
+}
+
+var qaBank = [instruction,qa1,qa2,qa3,qa4];
+
+
+
